7f061a5d02f21a1802d0a10fd05a553714dd06ed
wealthygogi <wealthygogi@gmail.com>
2025-03-11 00:08:46 +0900

    first init

---
+++ docs/게임/th06/daiyousei.md
@@ -0,0 +1,13 @@
+---
+sidebar_position: 4
+title: "대요정"
+description: "동방홍마향의 2면 중간보스"
+---
+
+## 대요정 (大妖精, Daiyousei)
+
+import daiyousei from './img/daiyousei.png';
+
+<img src={daiyousei} alt="img" style={{width: 250}} />
+
+-
---
+++ docusaurus.config.ts
@@ -0,0 +1,106 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "동방프로젝트 한글화 문서",
+  tagline: "동방 굿",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://touhouwiki-kr.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/touhouwiki-kr/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "touhou-kr", // Usually your GitHub org/user name.
+  projectName: "touhouwiki-kr", // Usually your repo name.
+  trailingSlash: false,
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "ko",
+    locales: ["ko", "ja"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/touhou-kr/touhouwiki-kr/tree/main/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "동방프로젝트 한글화 문서",
+      logo: {
+        alt: "동방프로젝트 한글화 문서",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "동방",
+        },
+        {
+          href: "https://github.com/touhou-kr/touhouwiki-kr",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "동방",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Editor",
+          items: [
+            {
+              label: "X",
+              href: "https://x.com/wealthygogi",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyleft © ${new Date().getFullYear()} @wealthygogi. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
