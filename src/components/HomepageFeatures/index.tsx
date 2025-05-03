694fa6310e9d9ce5a97c76ddf9802781fdb651e8
wealthygogi <wealthygogi@gmail.com>
2025-05-04 00:25:39 +0900

    fixup index page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,47 +1,43 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: '글 출처',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        https://www.thpatch.net/
-      </>
-    ),
+    title: "글 출처",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    description: <>https://www.thpatch.net/</>,
   },
   {
-    title: '그림 출처',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        https://x.com/dairi155
-      </>
-    ),
+    title: "그림 출처",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    description: <>https://x.com/dairi155</>,
   },
   {
-    title: 'License',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "License",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        CC BY-SA 4.0
+        <img
+          src={require("@site/static/img/cc-by-sa.png").default}
+          alt="CC BY-SA 4.0"
+          style={{ maxWidth: "100px", marginBottom: "0.5rem" }}
+        />
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
