28a8dc5a77432095744b18f7a5cc5d4b5c20a765
wealthygogi <wealthygogi@gmail.com>
2025-08-10 23:57:36 +0900

    update game items

--- src/components/GachaGame/data/gameItems.ts
+++ src/components/GachaGame/data/gameItems.ts
@@ -5,7 +5,33 @@ export interface GachaItem {
 }
 
 export const GAME_ITEMS: GachaItem[] = [
-  // 슈팅 게임
+  // 구작 슈팅 게임
+  {
+    id: "th01",
+    name: "동방영이전",
+    description: "동방영이전 ~ The Highly Responsive to Prayers",
+  },
+  {
+    id: "th02",
+    name: "동방봉마록",
+    description: "동방봉마록 ~ the Story of Eastern Wonderland",
+  },
+  {
+    id: "th03",
+    name: "동방몽시공",
+    description: "동방몽시공 ~ The Phantasmagoria of Dim.Dream",
+  },
+  {
+    id: "th04",
+    name: "동방환상향",
+    description: "동방환상향 ~ Lotus Land Story",
+  },
+  {
+    id: "th05",
+    name: "동방괴기담",
+    description: "동방괴기담 ~ Mystic Square",
+  },
+  // 신작 슈팅 게임
   {
     id: "th06",
     name: "동방홍마향",
@@ -77,15 +103,72 @@ export const GAME_ITEMS: GachaItem[] = [
     description: "동방수왕원 ~ Unfinished Dream of All Living Ghost.",
   },
 
+  // 외전
+  {
+    id: "th095",
+    name: "동방문화첩",
+    description: "동방문화첩 ~ Shoot the Bullet.",
+  },
+  {
+    id: "th125",
+    name: "더블 스포일러",
+    description: "더블 스포일러 ~ 동방문화첩",
+  },
+  {
+    id: "th128",
+    name: "요정대전쟁",
+    description: "요정대전쟁 ~ 동방삼월정",
+  },
+  {
+    id: "th143",
+    name: "탄막 아마노자쿠",
+    description: "탄막 아마노자쿠 ~ Impossible Spell Card.",
+  },
+  {
+    id: "th165",
+    name: "비봉 나이트메어 다이어리",
+    description: "비봉 나이트메어 다이어리 ~ Violet Detector.",
+  },
+  {
+    id: "th185",
+    name: "불릿필리아들의 암시장",
+    description: "불릿필리아들의 암시장 ~ 100th Black Market.",
+  },
+
   // 격투 게임
-  // {
-  //   id: "th075",
-  //   name: "동방췌몽상",
-  //   description: "동방췌몽상 ~ Immaterial and Missing Power.",
-  // },
-  // {
-  //   id: "th105",
-  //   name: "동방비상천",
-  //   description: "동방비상천 ~ Scarlet Weather Rhapsody.",
-  // },
+  {
+    id: "th075",
+    name: "동방췌몽상",
+    description: "동방췌몽상 ~ Immaterial and Missing Power.",
+  },
+  {
+    id: "th105",
+    name: "동방비상천",
+    description: "동방비상천 ~ Scarlet Weather Rhapsody.",
+  },
+  {
+    id: "th123",
+    name: "동방비상천칙",
+    description: "동방비상천칙 ~ 초대형 기뇰의 수수께끼를 쫓아서",
+  },
+  {
+    id: "th135",
+    name: "동방심기루",
+    description: "동방심기루 ~ Hopeless Masquerade.",
+  },
+  {
+    id: "th145",
+    name: "동방심비록",
+    description: "동방심비록 ~ Urban Legend in Limbo.",
+  },
+  {
+    id: "th155",
+    name: "동방빙의화",
+    description: "동방빙의화 ~ Antinomy of Common Flowers.",
+  },
+  {
+    id: "th175",
+    name: "동방강욕이문",
+    description: "동방강욕이문 ~ 수몰된 침수지옥",
+  },
 ];
